5db9dff6fa4127226832317a7cb0891d47c2ed4f
Jameela Suhana <jameelasuhana09@gmail.com>
2026-08-02 22:17:46 +0530

    Update backend URL

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth"
+  baseURL:  "https://smart-trip-backend-dopj.onrender.com/api/auth"
 });
 
 export const loginUser = (data) => API.post("/login", data);
--- frontend/src/api/trips.js
+++ frontend/src/api/trips.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/trips"
+ baseURL: "https://smart-trip-backend-dopj.onrender.com/api/trips" 
 });
 
 API.interceptors.request.use((req) => {
